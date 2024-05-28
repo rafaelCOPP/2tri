6b3348f15c3a145f1de0202e9b8d014e19d7b392
rafaelCOPP <128715716+rafaelCOPP@users.noreply.github.com>
2024-05-28 08:49:03 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixasAlternativas = document.querySelector(".caixa-alternativas")
+const caixaResultado = document.querySelector(".caixa-resultado")
+const textoResultado = document.querySelector(".texto-resultado")
 
+const perguntas = [
+    {
+        enunciado:"Você faria o tratamento da manual ?",
+        alternativas:[
+            "Sim",
+            "Não",
+        ]
+    },
+    {
+        enunciado:"Você usaria a insider ?",
+        alternativas:[
+            "Sim",
+            "Sim",
+        ]
+    },
+    {
+        enunciado:"Você ",
+        alternativas:[
+            "Alternativa1",
+            "Alternativa2",
+        ]
+    },
+    {
+        enunciado:"Pergunta4",
+        alternativas:[
+            "Alternativa1",
+            "Alternativa2",
+        ]
+    },
+    {
+        enunciado:"Pergunta5",
+        alternativas:[
+            "Alternativa1",
+            "Alternativa2",
+        ]
+    },
+];
